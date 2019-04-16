bdb1263075688c2fd863f9b6322f109620740b29
ys951 <ys951207@163.com>
2019-04-16 23:33:08 +0800

    cookie&session

---
+++ 1-2-Cookie/src/main/java/cn/ys/cookie/CookieDemo3.java
@@ -0,0 +1,30 @@
+package cn.ys.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * Cookie快速入门
+ *
+ */
+@WebServlet("/cookieDemo3")
+public class CookieDemo3 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.创建Cookie对象
+        Cookie c1 = new Cookie("msg2", "hello2");
+        Cookie c2 = new Cookie("name", "zhangsan");
+        //2.发送Cookie
+        response.addCookie(c1);
+        response.addCookie(c2);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
